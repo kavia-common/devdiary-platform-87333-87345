0f55ffd4a1d39363a1ed4ac873175be43fb1ad15
Kavia AI <production@kavia.ai>
2025-09-20 21:21:17 +0000

    CheckPoint - cge1bade45

---
+++ devdiary_backend/src/main/java/com/example/demo/devdiarybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devdiarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class devdiarybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(devdiarybackendApplication.class, args);
+	}
+
+}
